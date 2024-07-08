3384a30a21714a8b74ed451ec0ed8a74993994b9
ogedi001 <ogedifavour2@gmail.com>
2024-07-08 08:29:01 +0100

    second commit

---
+++ src/tests/token.spec.ts
@@ -0,0 +1,35 @@
+//import { generateJWT, verifyJwtToken, UserPayload } 
+
+import { generateJWT, Userpayload, verifyJwtToken } from "../helpers";
+
+describe('Token Generation and Verification', () => {
+  it('should generate a valid JWT token', () => {
+    const payload: Userpayload = {
+      id: '12345',
+      email: 'test@example.com',
+      firstname: 'John',
+      lastname: 'Doe',
+    };
+    const token = generateJWT(payload);
+    expect(token).toBeTruthy();
+
+    const decodedPayload = verifyJwtToken(token);
+    expect(decodedPayload.id).toBe(payload.id);
+    expect(decodedPayload.email).toBe(payload.email);
+    // Add more assertions as needed for other fields in the payload
+  });
+
+  it('should verify a valid JWT token', () => {
+    const payload: Userpayload= {
+      id: '12345',
+      email: 'test@example.com',
+      firstname: 'John',
+      lastname: 'Doe',
+    };
+    const token = generateJWT(payload);
+    const decodedPayload = verifyJwtToken(token);
+    expect(decodedPayload.id).toBe(payload.id);
+    expect(decodedPayload.email).toBe(payload.email);
+    // Add more assertions as needed for other fields in the payload
+  });
+});
